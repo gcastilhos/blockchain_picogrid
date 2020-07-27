5faf2dd39e298f79b371e6b7c52ad453befd87d1
Gustavo Garcia <gcastilhos@gmail.com>
2020-07-27 17:07:45 +1000

    Updated server.js

--- server.js
+++ server.js
@@ -10,23 +10,11 @@ const port = process.env.PORT || 80;
 const DATA_URI = process.env.DATA_URI || 'https://eventqueue.herokuapp.com/events';
 
 app.get('/events', (req, res) => {
-  console.log('Events requested');
   var jsonResponse;
   fetch(DATA_URI, {method: 'GET'}).
     then(res => res.json()).
     then((json) => {
-      let columns = [];
-      let data = [];
-      for (var key in json[0]) {
-        columns.push(key);
-        data.push(json[0][key]);
-      }
-      jsonResponse = {
-        columns: columns,
-        data: [data]
-      }
-      console.log(jsonResponse);
-      res.send(jsonResponse);
+      res.send(json);
     });
 });
 
