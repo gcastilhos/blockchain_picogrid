4bb9d0680eed9807697191943586c8d921ac4635
Gustavo Garcia <gcastilhos@gmail.com>
2020-07-28 06:56:48 +1000

    Updated store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,27 +1,43 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import sha256 from 'sha256'
+import encode from '@/encoder.js'
+import INITIAL_HASH from '@/mockdata.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    picogridTotals: []
+    picogridTotals: [],
+    previousHash: INITIAL_HASH
   },
   getters: {
     picogridsHash: state => {
-      let batchNumbers = new Map()
-      state.picogridTotals.forEach(item => {
-        if (batchNumbers.has(item[0])) {
-          batchNumbers.get(item[0]).push(item.slice(1))
-        } else {
-          batchNumbers.set(item[0], [item.slice(1)])
-        }
-      })
-      let bn = new Map()
-      for (const pair of batchNumbers.entries()) {
-        bn.set(pair[0], sha256(pair[1].sort((a, b) => b[0] < a[0]).map(a => JSON.stringify(a.slice(1))).join(',')))
+
+      let addPicogrid = () => {
+        let picogridValues = new Map()
+        state.picogridTotals.forEach(item => {
+          if (picogridValues.has(item[0])) {
+            picogridValues.get(item[0]).push(item.slice(1))
+          } else {
+            picogridValues.set(item[0], [item.slice(1)])
+          }
+        })
+        return picogridValues
       }
-      return bn
+
+      let createHash = (values) => {
+        let bn = new Map()
+        for (const pair of values.entries()) {
+          if (pair[1].length === 11) {
+            let sortedValuesAsString = pair[1].sort((a, b) => b[0] < a[0]).map(a => JSON.stringify(a.slice(1))).join(',')
+            let encodedValues = encode(sortedValuesAsString)
+            state.previousHash = encodedValues.hash
+            bn.set(pair[0], encodedValues.hash)
+          }
+        }
+        return bn
+      }
+
+      return createHash(addPicogrid())
     }
   },
   mutations: {
