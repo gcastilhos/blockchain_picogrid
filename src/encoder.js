0c4c20a2c11b83c03cd2f7b711a65150a8aefba0
Gustavo Garcia <gcastilhos@gmail.com>
2020-07-28 06:56:08 +1000

    Updated encode function

--- src/encoder.js
+++ src/encoder.js
@@ -1,8 +1,19 @@
 import sha256 from 'sha256';
 
-export default function encode(text) {
-  if (text !== '') {
-    return sha256(text);
+var isValidHash = (hashCode) => {
+   if (hashCode !== undefined) {
+     return hashCode.startsWith("000")
+   }
+   return false
+}
+
+export default function encode(text, previousHash) {
+  var nonce = -1
+  var hashCode = undefined
+  while (!isValidHash(hashCode)) {
+    nonce++
+    var textToEncode = previousHash + text + nonce
+    hashCode = sha256(textToEncode)
   }
-  return text;
+  return {"nonce": nonce, "hash": hashCode}
 }
